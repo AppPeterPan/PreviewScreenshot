4edfe91350c45628a1eb73a84dd2bf59b90cc3e4
peter <apppeterpan@gmail.com>
2023-02-04 17:53:36 +0800

    Initial Commit

---
+++ PreviewScreenshot/PreviewScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewScreenshotApp.swift
+//  PreviewScreenshot
+//
+//  Created by Peter Pan on 2023/2/4.
+//
+
+import SwiftUI
+
+@main
+struct PreviewScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
